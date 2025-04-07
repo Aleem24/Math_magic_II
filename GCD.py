5c8cb23337c207b5b3c74f5b5a1ebd62da04d018
Aleem24 <md.samiulaleem16@gmail.com>
2025-04-07 19:59:08 +0600

    Done Git💕

--- GCD.py
+++ GCD.py
@@ -5,21 +5,26 @@ num1 = int(input("Enter the first number : "))
 num2 = int(input("Enter the second number :"))
 
 # Euclidian Algorithm of finding the GCD or HCF
+def gcd(num1,num2):
+        while(num1):
+         temp = num1
+         num1  = num2%num1
+         num2 = temp
+        return num2
+        
+lcm = (num1*num2)/gcd(num1,num2)
 
-if(num1 < num2):
-    while(num1):
-        temp = num1
-        num1  = num2%num1
-        num2 = temp
-        print(f"The GCD between {num1} and {num2} is : {num2}")
+print(f"The LCM is = {lcm}")
 
-elif(num2<num1): 
-    while(num2):
-        temp = num2
-        num2  = num1%num2
-        num1 = temp
-        print(f"The GCD between {num1} and {num2} is : {num1}")
-else:
-     print(f"The GCD between {num1} and {num2} is : {num1}")
+    
+
+
+
+
+
+
+
+
+
      
 
